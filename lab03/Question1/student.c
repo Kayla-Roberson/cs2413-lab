4193178dca4e016414ec802639fa916e70e971e4
Kayla-Roberson <kaylajaneroberson@gmail.com>
2026-02-24 09:47:33 -0600

    Lab3 Complete

--- lab03/Question1/student.c
+++ lab03/Question1/student.c
@@ -26,5 +26,14 @@ struct ListNode* reverseList(struct ListNode* head) {
     // Hints (optional):
     // - Keep track of previous and current pointers.
     // - Iterate until current becomes NULL.
-
+    struct ListNode* prev = NULL;
+    struct ListNode* current = head;
+    struct ListNode* next = NULL;
+    while (current != NULL){
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
 }
--- lab03/Question2/student.c
+++ lab03/Question2/student.c
@@ -14,4 +14,18 @@
 
 struct ListNode* swapPairs(struct ListNode* head) {
       // TODO: implement
+      struct ListNode* next = head;
+      struct ListNode* prev = NULL;
+      struct ListNode* head_new = head;
+      while(next != NULL && next->next != NULL){
+            struct ListNode* one = next;
+            struct ListNode* two = one->next;
+            next = two->next;
+            if(prev == NULL){head_new = two;}
+            else{prev->next = two;}
+            two->next = one;
+            one->next = next;
+            prev = one;
+      }
+      return head_new;
 }
