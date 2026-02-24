c8a57df33d020e9f5ac55a96b96b4bb69ad1c04c
Kayla-Roberson <kaylajaneroberson@gmail.com>
2026-02-24 09:47:33 -0600

    Lab3 Complete

--- lab03/Question3/student.c
+++ lab03/Question3/student.c
@@ -14,5 +14,12 @@
 
 bool hasCycle(struct ListNode *head) {
     // TODO: implement
-
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+    while(fast!=NULL&&fast->next !=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) return true;
+    }
+    return false;
 }
--- lab03/Question4/student.c
+++ lab03/Question4/student.c
@@ -14,5 +14,11 @@
 
 int middleNodeValue(struct ListNode *head) {
     // TODO: implement
-
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+    while(fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow->val;
 }
